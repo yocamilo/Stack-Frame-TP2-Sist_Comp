42654795868040ad45350981da9e63f2584b6777
valentinezequiel <64497132+valentinezequiel@users.noreply.github.com>
2023-04-12 22:04:54 -0300

    se agregan comentarios, resolver dudas

--- main.asm
+++ main.asm
@@ -5,8 +5,8 @@ main:
     mov ebp,esp
 
     ;pusha ;guardar gp registers???
-    fld dword   [ebp+8]
-    fmul dword  [ebp+12]
+    fld dword   [ebp+8] ;load floating-point value in st0 porque +8??
+    fmul dword  [ebp+12];multiplica lo que tenia en st0 por el otro parametro y resultado queda en st0
 
     leave
     ret
