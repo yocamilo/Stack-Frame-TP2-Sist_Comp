e3cf855760ba2d8dcb5277db14db4f5ea4d22814
valentinezequiel <valentin.ruiz@mi.unc.edu.ar>
2023-04-21 19:55:17 -0300

    no era necesario guardar espacio para variables locales, pues no se utiliza ninguna

--- mult.asm
+++ mult.asm
@@ -3,7 +3,7 @@ section .text
     global multiply_floats
 
 multiply_floats:
-    enter 8, 0          ; establecer un marco de pila de 8 bytes
+    enter 0, 0          ; salvar viejo EBP, y luego EBP=ESP
 
     fld dword [ebp + 8]     ;carga el valor del primer argumento cripto_value_usd en st0.
     fmul dword [ebp + 12]   ;multiplica el valor en st0 por segundo argumento usd_to_change, el resultado se almacena en st0.
